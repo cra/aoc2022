4ed1784490c551a56d3f00771999d7ec10490353
Igor Mosyagin <408698+cra@users.noreply.github.com>
2022-12-16 16:53:44 +0100

    Solve day10 part1

---
+++ 10/solve.py
@@ -0,0 +1,172 @@
+test = """
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+""".lstrip()
+
+
+def get_xx(lines):
+    i, x, xx = 1, 1, {}
+    for line in lines:
+        xx[i] = x
+        line = line.strip()
+        if line == 'noop':
+            i += 1
+        else:
+            val = int(line.split()[-1])
+            xx[i+1] = x
+            x += val
+            i += 2
+
+
+def solve1(lines):
+    xx = get_xx(lines)
+    checks = (20, 60, 100, 140, 180, 220)
+    return sum(check * xx[check] for check in checks)
+
+
+print("Test1", solve1(test.splitlines()))
+print("Puzzle1", solve1(open("puzzle_input").readlines()))
